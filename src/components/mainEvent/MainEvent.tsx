9d900d8f29c8bce8f5e025417d2ea0397ad043f6
Mawio <marioimpesi1@gmail.com>
2023-08-18 00:45:34 +0200

    Main event tag is also responsive to height

--- src/components/mainEvent/MainEvent.tsx
+++ src/components/mainEvent/MainEvent.tsx
@@ -3,22 +3,13 @@ import Background from './Background';
 import "./MainEvent.css"
 import React, { useEffect, useState } from 'react';
 
-// Create an interface for the size of the window
-interface Size {
-  width: number;
-  height: number;
-}
-
 function MainEvent() {
 
-  const [size, setSize] = useState<Size>({width: window.innerWidth, height: window.innerHeight});
+  const [zoom, setZoom] = useState<number>(Math.min(window.innerHeight / 1080, window.innerWidth / 1920));
 
   // This function updates the state thus re-render components
   const resizeHandler = () => {
-    setSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
+    setZoom(Math.min(window.innerHeight / 1080, window.innerWidth / 1920));
   };
 
   // Listening for the window resize event
@@ -35,7 +26,7 @@ function MainEvent() {
   return (
     <>
       <Background />
-      <div id='main-event-tag' style={{zoom: size.width / 1920}}>
+      <div id='main-event-tag' style={{zoom: zoom}}>
         <Typography color={'white'} fontSize={"70px"} textAlign={"center"}> British Grand Prix </Typography>
       </div>
     </>
